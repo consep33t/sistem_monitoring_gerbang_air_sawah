f919ffd8a8c21b9220c62de841889aba8c16cddd
Consep33t <agengp360@gmail.com>
2025-07-07 22:15:19 +0700

    fix connection db

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2/promise";
 export const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "consep33t",
+  password: "Consep33t",
   database: "sistem_monitoring_gerbang_air",
 });
