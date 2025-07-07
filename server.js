b5cd4590350605b84d52fb0ea2880e4d43b2e7c0
Consep33t <agengp360@gmail.com>
2025-07-07 19:35:17 +0700

    setup ip on vps

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.prepare().then(() => {
   });
 
   server.listen(3000, () => {
-    console.log("✅ Server berjalan di http://localhost:3000");
+    console.log("✅ Server berjalan di http://localhost:85");
   });
 
   setupSocket(server);
