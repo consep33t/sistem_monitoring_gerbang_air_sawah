95b6879b5eea353da3381023e5e7e2e3474492f5
Consep33t <agengp360@gmail.com>
2025-09-18 21:28:00 +0700

    is

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = createServer((req, res) => {
+    // Middleware CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    // Tangani preflight request (OPTIONS) dari browser atau klien
+    if (req.method === "OPTIONS") {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
+
     handle(req, res);
   });
 
